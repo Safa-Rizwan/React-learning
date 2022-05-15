9e9fbc85d8119b697db8ce9cc7c449fbd69b004b
Safa Rizwan <103735300+Safa-Rizwan@users.noreply.github.com>
2022-05-15 21:01:56 +0500

    JSX completed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,9 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="blank">
-      
+    <>
+    <nav>
+      <li>home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    <div className="container">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus architecto pariatur, deleniti provident omnis expedita officia labore placeat doloremque. Dolore et obcaecati quidem culpa tempora illum aut omnis vitae cupiditate saepe, nobis quisquam ipsum?</p>
     </div>
+    </>
   );
 }
 
